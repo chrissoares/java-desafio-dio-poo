595f4edaea452c069d283b203fca4948dfa000c5
chrissoares <chrisrsoares@gmail.com>
2023-01-28 14:47:25 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso cursoJava = new Curso("Curso Java", "Descrição do curso Java", 8);
+        Curso cursoJs = new Curso("Curso JS", "Descrição do curso JS", 4);
+       
+        Mentoria mentoria = new Mentoria("Mentoria Java", "Descrição mentoria Java", LocalDate.now());
+
+        System.out.println(cursoJava);
+    }
+}
