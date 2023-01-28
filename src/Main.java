5c8f736916e0840a05dec05289629786e826f5e7
chrissoares <chrisrsoares@gmail.com>
2023-01-28 14:59:46 -0300

    Polimorfismo, classe Conteudo

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Curso cursoJava = new Curso("Curso Java", "Descrição do curso Java", 8);
         Curso cursoJs = new Curso("Curso JS", "Descrição do curso JS", 4);
-       
+      
         Mentoria mentoria = new Mentoria("Mentoria Java", "Descrição mentoria Java", LocalDate.now());
 
         System.out.println(cursoJava);
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,31 +1,15 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
 
     public Curso(String titulo, String descricao, int cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
+        this.setTitulo(titulo);
+        this.setDescricao(descricao);
         this.cargaHoraria = cargaHoraria;
     }
     
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -35,11 +19,16 @@ public class Curso {
 
     @Override
     public String toString() {
-        return "{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + cargaHoraria +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
 }
